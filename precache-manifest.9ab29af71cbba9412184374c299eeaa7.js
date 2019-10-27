72dad714a60041a637f4e1d00ed66249d1f2d0cf
Pyi Them Kyaw <pyithemkyaw@Pyis-MacBook-Pro.local>
2019-10-27 15:46:32 -0500

    Updates

---
+++ precache-manifest.9ab29af71cbba9412184374c299eeaa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652a40defdc91796bfd3894d17823762",
+    "url": "/ctf-challenge/index.html"
+  },
+  {
+    "revision": "2b4ee016e133a37e6c5f",
+    "url": "/ctf-challenge/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "93c4cdfd24812c142094",
+    "url": "/ctf-challenge/static/css/main.7dfa905d.chunk.css"
+  },
+  {
+    "revision": "2b4ee016e133a37e6c5f",
+    "url": "/ctf-challenge/static/js/2.b7830c6d.chunk.js"
+  },
+  {
+    "revision": "93c4cdfd24812c142094",
+    "url": "/ctf-challenge/static/js/main.292a8844.chunk.js"
+  },
+  {
+    "revision": "8911c0884e8ffce5c877",
+    "url": "/ctf-challenge/static/js/runtime-main.5b05ccf6.js"
+  }
+]);
