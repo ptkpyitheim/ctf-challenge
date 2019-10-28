0bc21dd51b7260c0cde58ed050723a3d20c0dc20
Pyi Them Kyaw <pyithemkyaw@Pyis-MacBook-Pro.local>
2019-10-27 23:13:35 -0500

    changed lvl4

--- src/components/Level3/index.js
+++ src/components/Level3/index.js
@@ -38,7 +38,7 @@ class Level3 extends React.Component {
         if(this.state.answer === "flag{console_can_help_you_debug_a_lot}") {
             alert("Success!");
             //Reroute to next page
-            this.props.history.push('/level3');
+            this.props.history.push('/level4');
         }
         else {
             this.setState({
