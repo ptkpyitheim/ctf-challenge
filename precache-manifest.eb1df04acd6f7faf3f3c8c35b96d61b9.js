1bf648199b4c1566c87fb02f2d11a0dd141c73a1
Pyi Them Kyaw <pyithemkyaw@Pyis-MacBook-Pro.local>
2019-11-02 16:34:59 -0500

    Updates

--- precache-manifest.eb1df04acd6f7faf3f3c8c35b96d61b9.js
+++ precache-manifest.eb1df04acd6f7faf3f3c8c35b96d61b9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "146130d8b56289fc1674026115b60820",
+    "revision": "e17375d71cb82450cb00c98355995c00",
     "url": "/ctf-challenge/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ctf-challenge/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "ee5a08204c1242e31e72",
+    "revision": "2aebaf6c046c35ea3b8a",
     "url": "/ctf-challenge/static/css/main.e25eca03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ctf-challenge/static/js/2.b6f608ab.chunk.js"
   },
   {
-    "revision": "ee5a08204c1242e31e72",
-    "url": "/ctf-challenge/static/js/main.41903721.chunk.js"
+    "revision": "2aebaf6c046c35ea3b8a",
+    "url": "/ctf-challenge/static/js/main.ba5c9979.chunk.js"
   },
   {
     "revision": "8911c0884e8ffce5c877",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ctf-challenge/precache-manifest.585187e03a314284b51fc629ab3465dd.js"
+  "/ctf-challenge/precache-manifest.eb1df04acd6f7faf3f3c8c35b96d61b9.js"
 );
 
 self.addEventListener('message', (event) => {
